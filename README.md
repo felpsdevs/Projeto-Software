3c239f2880cc7c4928fbc19758e40f1235a2b7fa
felpsdevs <felipemartins020297@gmail.com>
2024-10-05 00:54:39 -0300

    ok

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Tecnologias Utilizadas:
 
 Frontend: HTML, CSS
 Backend: Python (com Flask)
-Banco de Dados: SQLite / Novo banco
+Banco de Dados: Postgree
 Descrição: É um aplicativo web que permite aos usuários adicionar, visualizar e excluir tarefas. Ele consiste em uma página HTML para a interface do usuário, código Python com Flask para a lógica de backend, e um banco de dados para armazenar os dados das tarefas.
--- app.py
+++ app.py
@@ -1,36 +1,52 @@
-from flask import Flask, request, redirect, render_template
-import sqlite3
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
+import psycopg2
+from psycopg2 import sql
 
 def get_db_connection():
-    conn = sqlite3.connect('tasks.db')
-    conn.row_factory = sqlite3.Row
+    conn = psycopg2.connect(
+        dbname='postgres_db',  
+        user='postgres',       
+        password='970202',     
+        host='localhost',      
+        port='5432'           
+    )
     return conn
+
+app = Flask(__name__)  
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    tasks = conn.execute('SELECT * FROM tasks').fetchall()
+    cur = conn.cursor()
+    cur.execute('SELECT id, name FROM tasks')  
+    tasks = cur.fetchall()  
+    cur.close()
     conn.close()
-    return render_template('index.html', tasks=tasks)
+
+    tasks_list = [{'id': task[0], 'name': task[1]} for task in tasks]
+    return render_template('index.html', tasks=tasks_list)
+
 
 @app.route('/add', methods=('POST',))
 def add():
     task = request.form['task']
     conn = get_db_connection()
-    conn.execute('INSERT INTO tasks (name) VALUES (?)', (task,))
+    cur = conn.cursor()
+    cur.execute('INSERT INTO tasks (name) VALUES (%s)', (task,))
     conn.commit()
+    cur.close()
     conn.close()
     return redirect('/')
 
 @app.route('/delete/<int:task_id>', methods=('POST',))
 def delete(task_id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+    cur = conn.cursor()
+    cur.execute('DELETE FROM tasks WHERE id = %s', (task_id,))
     conn.commit()
+    cur.close()
     conn.close()
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
