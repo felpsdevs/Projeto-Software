94b81de30f5858fe47539933e078eef65710a2bf
felpsdevs <felipemartins020297@gmail.com>
2024-10-14 11:45:24 -0300

    okay

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect
 import psycopg2
-from psycopg2 import sql
 
 def get_db_connection():
     conn = psycopg2.connect(
         dbname='postgres_db',  
         user='postgres',       
         password='970202',     
-        host='localhost',      
+        host='localhost',          
         port='5432'           
     )
     return conn
@@ -18,12 +17,12 @@ app = Flask(__name__)
 def index():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT id, name FROM tasks')  
+    cur.execute('SELECT id, name, completed FROM tasks') 
     tasks = cur.fetchall()  
     cur.close()
     conn.close()
 
-    tasks_list = [{'id': task[0], 'name': task[1]} for task in tasks]
+    tasks_list = [{'id': task[0], 'name': task[1], 'completed': task[2]} for task in tasks] 
     return render_template('index.html', tasks=tasks_list)
 
 
@@ -32,7 +31,7 @@ def add():
     task = request.form['task']
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('INSERT INTO tasks (name) VALUES (%s)', (task,))
+    cur.execute('INSERT INTO tasks (name, completed) VALUES (%s, FALSE)', (task,)) 
     conn.commit()
     cur.close()
     conn.close()
@@ -48,5 +47,16 @@ def delete(task_id):
     conn.close()
     return redirect('/')
 
+
+@app.route('/concluir/<int:task_id>', methods=['POST'])
+def concluir_tarefa(task_id):
+    conn = get_db_connection() 
+    cur = conn.cursor()
+    cur.execute("UPDATE tasks SET completed = TRUE WHERE id = %s", (task_id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect('/')
+
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
